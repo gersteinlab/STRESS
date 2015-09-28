5531ceacf877480d8137a9d9ca0b00efb30124f7
Shantao Li <shantaolee@gmail.com>
2015-09-28 10:55:51 -0400

    moved Sim to heap

--- surface_residue/code_pack/surfaceProbe.c
+++ surface_residue/code_pack/surfaceProbe.c
@@ -1,3 +1,45 @@
+/*
+ * University of Illinois Open Source License
+ * Copyright 2009, 2011, 2012 Luthey-Schulten Group, 
+ * All rights reserved.
+ * 
+ * Developed by: Luthey-Schulten Group
+ *                           University of Illinois at Urbana-Champaign
+ *                           http://www.scs.uiuc.edu/~schulten
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the Software), to deal with 
+ * the Software without restriction, including without limitation the rights to 
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ * of the Software, and to permit persons to whom the Software is furnished to 
+ * do so, subject to the following conditions:
+ * 
+ * - Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimers.
+ * 
+ * - Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimers in the documentation 
+ * and/or other materials provided with the distribution.
+ * 
+ * - Neither the names of the Luthey-Schulten Group, University of Illinois at
+ * Urbana-Champaign, nor the names of its contributors may be used to endorse or
+ * promote products derived from this Software without specific prior written
+ * permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL 
+ * THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+ * OTHER DEALINGS WITH THE SOFTWARE.
+ *
+ * Author(s): Anurag Sethi, John Eargle
+ * Modifications by: Shantao Li, Declan Clarke (lab of Mark Gerstein, 
+ * Yale University, 2015)
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -1150,12 +1192,19 @@ void sort(bindingSite *allSites, float *LC, int nSim)
 void mergeBindingSites(bindingSite *allSites, int Ntrials, float *LC)
 {
 	int i, j, k;
-	float Sim[Ntrials][Ntrials];
+	// float Sim[Ntrials][Ntrials];
+	float **Sim;
 	float max = 0.;
 	int mergingSites;
 	
+	Sim = calloc(sizeof(float*), Ntrials);
+	for (i=0; i<Ntrials; i++) 
+		Sim[i] = calloc(sizeof(float), Ntrials);
+
 	printf("->merging Sites\n");
 	
+	
+
 	k = 0;
 	for (i=0; i<Ntrials; i++)
 	{
@@ -1214,6 +1263,11 @@ void mergeBindingSites(bindingSite *allSites, int Ntrials, float *LC)
 	}
 	
 	printf("<-merging Sites\n");
+
+	for (i=0; i<Ntrials; i++)
+		free(Sim[i]);
+	free(Sim);
+
 	return;
 }				
 
